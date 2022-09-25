5f715a77e4278fb6f80065c912c0b30d4032b1a8
Cezary Kochański <cezarykochanski@Air-Cezary.wtvk.pl>
2022-09-25 13:59:16 +0200

    Add test for array example

--- src/main/java/pl/sda/ArrayExample.java
+++ src/main/java/pl/sda/ArrayExample.java
@@ -4,9 +4,21 @@ public class ArrayExample {
 
     public static String[] removeDuplicates(String[] strings) {
         String[] removedDuplicatesArray = new String[0];
-        for(String string : strings) {
-
+        for (String original : strings) {
+            boolean isAlreadyInArray = false;
+            for (String string : removedDuplicatesArray){
+                if (original.equals(string)){
+                    isAlreadyInArray = true;
+                    break;
+                }
+            }
+            if (!isAlreadyInArray) {
+                int current = removedDuplicatesArray.length;
+                removedDuplicatesArray = new String[current + 1];
+                removedDuplicatesArray[current] = original;
+            }
         }
+        return removedDuplicatesArray;
     }
 
 }
