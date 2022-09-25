54cf8871681234548cd88c3df07245e16ca762ba
Cezary Kochański <cezarykochanski@Air-Cezary.wtvk.pl>
2022-09-25 13:16:08 +0200

    Add other operations

--- src/main/java/pl/sda/Calculator.java
+++ src/main/java/pl/sda/Calculator.java
@@ -1,7 +1,24 @@
 package pl.sda;
 
 public class Calculator {
-    public int add(int a, int b){
+
+    public int add(int a, int b) {
         return a + b;
     }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public double division(double a, double b) {
+        if (b == 0) {
+            return -1;
+        }
+        return a / b;
+    }
+
 }
--- src/test/java/pl/sda/CalculatorTest.java
+++ src/test/java/pl/sda/CalculatorTest.java
@@ -6,18 +6,76 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
 
+    private final Calculator calculator = new Calculator();
+
     @Test
     void shouldAddTwoNumbers() {
         //given
         int a = 10;
         int b = 15;
-        Calculator calculator = new Calculator();
 
         //when
         int result = calculator.add(a, b);
 
         //then
-        assertThat(result).isEqualTo(25);
+        assertThat(result)
+                .isEqualTo(25);
+    }
+
+    @Test
+    void shouldSubtractTwoNumbers() {
+        //given
+        int a = 15;
+        int b = 10;
+
+        //when
+        int result = calculator.subtract(a, b);
+
+        //then
+        assertThat(result)
+                .isEqualTo(5);
+    }
+
+    @Test
+    void shouldMultiplyTwoNumbers() {
+        //given
+        int a = 2;
+        int b = 4;
+
+        //when
+        int result = calculator.multiplication(a, b);
+
+        //then
+        assertThat(result)
+                .isEqualTo(8);
+    }
+
+    @Test
+    void shouldDivideTwoNumbers() {
+        //given
+        double a = 9;
+        double b = 2;
+
+        //when
+        double result = calculator.division(a, b);
+
+        //then
+        assertThat(result)
+                .isEqualTo(4.5);
+    }
+
+    @Test
+    void shouldReturnMinusOneWhenDivideByZero() {
+        //given
+        double a = 9;
+        double b = 0;
+
+        //when
+        double result = calculator.division(a, b);
+
+        //then
+        assertThat(result)
+                .isEqualTo(-1);
     }
 
 }
